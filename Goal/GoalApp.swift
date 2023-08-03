33b3c2f189c16a733dcf8371487dc75b6865ab6b
bmoxey <bmoxey@gmail.com>
2023-08-03 17:31:39 +1000

    Initial Commit

---
+++ Goal/GoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalApp.swift
+//  Goal
+//
+//  Created by Brett Moxey on 3/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
