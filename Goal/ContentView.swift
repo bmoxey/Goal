4eabedc6c028be8eda3ccb7473b43e21115f7cf5
bmoxey <bmoxey@gmail.com>
2023-08-03 18:03:46 +1000

    Initial

--- Goal/ContentView.swift
+++ Goal/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            ContainerRelativeShape()
+                .fill(Color.blue.gradient)
+                .ignoresSafeArea()
+            VStack {
+                VStack {
+                    Text("Game of a life")
+                        .font(.system(size:48, weight: .semibold))
+                        .foregroundColor(.white)
+                        
+                    Text("by Brett Moxey")
+                        .font(.system(size:24))
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical, 50)
+                Button {
+                    print("Doit")
+                } label: {
+                    HStack {
+                        Text("🤰")
+                            .font(.system(size:64))
+                        Text("Create a new life")
+                            .font(.system(size:32))
+                            .foregroundColor(.black)
+                            .frame(width:160)
+
+                    }
+                    .frame(width: 280, height:120)
+                    .background(Color.white.gradient)
+                    .cornerRadius(10.0)
+                    .padding(.bottom, 50)
+                }
+                Button {
+                    print("Go to graves")
+                } label: {
+                    HStack {
+                        Text("🪦")
+                            .font(.system(size:64))
+                        Text("Visit grave stones")
+                            .font(.system(size:32))
+                            .foregroundColor(.black)
+                            .frame(width:160)
+                    }
+                    .frame(width: 280, height:120)
+                    .background(Color.white.gradient)
+                    .cornerRadius(10.0)
+                    .padding(.bottom, 50)
+                }
+                Spacer()
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
