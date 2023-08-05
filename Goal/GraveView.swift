752bbcd7012a2c226b992b55563acf7a68d7a5c2
bmoxey <bmoxey@gmail.com>
2023-08-06 07:46:25 +1000

    Split views

---
+++ Goal/GraveView.swift
@@ -0,0 +1,20 @@
+//
+//  GraveView.swift
+//  Goal
+//
+//  Created by Brett Moxey on 6/8/2023.
+//
+
+import SwiftUI
+
+struct GraveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraveView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraveView()
+    }
+}
